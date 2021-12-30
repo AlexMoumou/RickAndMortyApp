1b994b651f246bcc10399a5dcadc08d5c25db4a2
alexmoumoulidis <alexmoumoulides@gmail.com>
2021-12-30 14:52:34 +0200

    Initial Commit

---
+++ RickAndMortySwiftUI/RickAndMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySwiftUIApp.swift
+//  RickAndMortySwiftUI
+//
+//  Created by Alex Moumoulides on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
